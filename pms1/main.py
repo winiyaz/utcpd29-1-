6d3da01c1c903f0da000a7be20e1b4a77aa8f52a
winiyaz <winiyaz@proton.me>
2024-08-22 07:52:02 -0700

    👌- UTCPD 29 - Setup

--- pms1/main.py
+++ pms1/main.py
@@ -1,5 +1,20 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Panty Manager')
+window.configure(bg="#020617", padx=200, pady=100)
+
+# --- Setup Canvas
+
+canvas = Canvas(height=200, width=200, highlightthickness=0)
+logo_img = PhotoImage(file="n.png")
+canvas.create_image(100,100, image=logo_img)
+canvas.pack()
+
+# -- Window Setup ---
+window.mainloop()
