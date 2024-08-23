5a10a7aad17dbd3727a8832c23a2f8c82aa3a429
winiyaz <winiyaz@proton.me>
2024-08-23 06:49:16 -0700

    👌- UTCPD 29 - Formatting contents of file being written

--- pms1/main.py
+++ pms1/main.py
@@ -1,4 +1,6 @@
+# Importing  packages
 from tkinter import *
+from tkinter import messagebox
 
 # -- Color Constants
 MAINBG = "#020617"
@@ -12,9 +14,8 @@ INSERT_BACKGROUND = 'hotpink'
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-import os # For writing directory
-from datetime import datetime, timezone # for getting current date and time
-
+import os  # For writing directory
+from datetime import datetime, timezone  # for getting current date and time
 
 # Ensure directory called 'panty' is present , if not create
 os.makedirs('panty', exist_ok=True)
@@ -24,21 +25,30 @@ def save():
 	website = website_entry.get()
 	email = email_entry.get()
 	password = password_entry.get()
-	current_datetime_utc = datetime.now(timezone.utc) # get current data and time UTCTZ
+	current_datetime_utc = datetime.now(timezone.utc)  # get current data and time UTCTZ
 
-	with open('panty/sniff.txt', "a") as data_file:
-		data_file.write(f"""
-***************************
-UTC - {current_datetime_utc}
----
-* Website  = {website}   
-* Email    = {email} 
-* Password = {password} 
-**************************
+	is_ok = messagebox.askokcancel(title=website, message=f"""
+	Details Entered:
+	website: {website}
+	Email:{email}
+	Password: {password}
+	---
+	Good ? 	
 """)
-		website_entry.delete(0, END)
-		password_entry.delete(0, END)
-		email_entry.delete(0, END)
+
+	if is_ok:
+		with open('panty/sniff.txt', "a") as data_file:
+			data_file.write(f"""
+	***************************
+	UTC - {current_datetime_utc}
+	---
+	* Website  = {website}   
+	* Email    = {email} 
+	* Password = {password} 
+	**************************
+	""")
+			website_entry.delete(0, END)
+			password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -53,7 +63,7 @@ logo_img = PhotoImage(file="n.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1, pady=20)
 
-# - Labels
+# --- Labels ---
 
 # Label Styles object
 label_style = {
@@ -69,8 +79,6 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password  ", **label_style)
 password_label.grid(row=3, column=0)
 
-# Entries
-
 # Entry styles made into an object that is then being called inside Entry
 entry_style = {
 	'width': 35,
@@ -83,6 +91,7 @@ entry_style = {
 	'highlightthickness': 1
 }
 
+# --- Entry Boxes --
 website_entry = Entry(**entry_style)
 website_entry.focus()
 website_entry.grid(row=1, column=1, pady=5, columnspan=2, ipady=10)
@@ -92,7 +101,7 @@ email_entry.insert(0, 'booty@sniff.com')
 password_entry = Entry(**entry_style)
 password_entry.grid(row=3, column=1, pady=5, ipady=10)
 
-# Buttons
+# --- Buttons ---
 generate_password_button = Button(text="GENPWD", font=('Arial', 15), bg=BUTTONBG, fg=ENTRYFG)
 generate_password_button.grid(row=3, column=3)
 add_button = Button(text="ADD", font=('Arial', 15), bg=BUTTONBG, fg=ENTRYFG, width=46, command=save)
