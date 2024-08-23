e3a389bd25e814431ef9215fdc243efe7821d92d
winiyaz <winiyaz@proton.me>
2024-08-23 04:36:46 -0700

    👌- UTCPD 29 - Convert Entry and Label styles into their own object

--- pms1/main.py
+++ pms1/main.py
@@ -3,9 +3,10 @@ from tkinter import *
 # -- Color Constants
 MAINBG = "#020617"
 TEXTFG = "#EEE4B1"
-ENTRYBG = "#1E201E"
+ENTRYBG = "black"
 ENTRYFG = "#F97300"
 BUTTONBG = "#180161"
+INSERT_BACKGROUND = 'hotpink'
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -25,24 +26,46 @@ canvas.grid(row=0, column=1, pady=20)
 
 # - Labels
 
-website_label = Label(text="Website  ", bg=MAINBG, fg=TEXTFG, font=('Arial', 20))
+# Label Styles object
+label_style = {
+	'bg': MAINBG,
+	'fg': TEXTFG,
+	'font': ('Arial', 20)
+}
+
+
+website_label = Label(text="Website  ", **label_style)
 website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username  ", bg=MAINBG, fg=TEXTFG, font=('Arial', 20))
+email_label = Label(text="Email/Username  ", **label_style)
 email_label.grid(row=2, column=0)
-password_label = Label(text="Password  ", bg=MAINBG, fg=TEXTFG, font=('Arial', 20))
+password_label = Label(text="Password  ", **label_style)
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35, bg=ENTRYBG, fg=ENTRYFG, font=('Courier', 20), insertbackground="hotpink", justify="center")
+
+# Entry styles made into an object that is then being called inside Entry
+entry_style = {
+	'width' : 35,
+	'bg': ENTRYBG,
+	'fg': ENTRYFG,
+	'font': ('Courier', 20),
+	'insertbackground': INSERT_BACKGROUND,
+	'justify': 'center',
+	'highlightcolor': INSERT_BACKGROUND,
+	'highlightthickness': 1
+}
+
+website_entry = Entry(**entry_style)
+website_entry.focus()
 website_entry.grid(row=1, column=1, pady=5, columnspan=2, ipady=10)
-email_entry = Entry(width=35, bg=ENTRYBG, fg=ENTRYFG, font=('Courier', 20), insertbackground="hotpink", justify="center")
+email_entry = Entry(**entry_style)
 email_entry.grid(row=2, column=1, pady=5, columnspan=2, ipady=10)
-password_entry = Entry(width=30, bg=ENTRYBG, fg=ENTRYFG, font=('Courier', 20), insertbackground="hotpink", justify="center")
+password_entry = Entry(**entry_style)
 password_entry.grid(row=3, column=1, pady=5, ipady=10)
 
 # Buttons
 generate_password_button = Button(text="Sniff", font=('Arial', 15), bg=BUTTONBG, fg=ENTRYFG)
-generate_password_button.grid(row=3, column=2)
+generate_password_button.grid(row=3, column=3)
 add_button = Button(text="ADD", font=('Arial', 15), bg=BUTTONBG, fg=ENTRYFG, width=46)
 add_button.grid(row=4, column=1, pady=20, columnspan=2)
 
